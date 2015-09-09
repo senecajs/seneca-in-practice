58776385a2052e4025efeb793086ba56be04ee3c
marcopiraccini <marco.piraccini@gmail.com>
2015-09-09 17:08:22 +0100

    Create empty problems

--- runners.js
+++ runners.js
@@ -1,3 +1,14 @@
 var runner = require('adventure-runner');
 
-runner('seneca-in-practice',[ '1.sum', '2.client']);
+runner('seneca-in-practice',[ '1.sum',
+                            '2.client',
+                            '3.roles',
+                            '4.extend',
+                            '5.extend-client',
+                            '6.multiple',
+                            '7.override',
+                            '8.transport',
+                            '9.more-transport',
+                            '10.mem-store',
+                            '11.plugin',
+                            ]);
