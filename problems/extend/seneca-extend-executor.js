5d39260e983c8a3817f0f216fbf65b4721460e1a
Michele Capra <michelecapra83@gmail.com>
2015-10-19 16:50:52 +0200

    client extended exercise and few minor fixes

--- problems/extend/seneca-extend-executor.js
+++ problems/extend/seneca-extend-executor.js
@@ -5,7 +5,7 @@
  * The module to be require as the first param.
  */
 var solution = require(process.argv[2])
-solution.act({role: 'math', cmd: 'sum', integer:true, left: 1, right: 2}, function (err, result) {
+solution.act({role: 'math', cmd: 'sum', integer:true, left: 1.5, right: 2.5}, function (err, result) {
   if (err) return console.error(err)
   console.log(result)
 })
---
+++ problems/extend_client/solution/math.js
@@ -0,0 +1,12 @@
+module.exports = function math( options ) {
+  this.add('role:math, cmd:sum', function( msg, respond ) {
+    var sum = msg.left + msg.right;
+    respond(null, {answer: sum});
+  });
+
+  this.add('role:math, cmd:sum, integer: true', function( msg, respond ) {
+    var sum = Math.floor(msg.left) + Math.floor(msg.right);
+    respond(null, {answer: sum});
+  });
+};
+
