0281f2cf206c6f6e0866ff344f6544425506c5eb
Marco Piraccini <marco.piraccini@gmail.com>
2016-05-09 21:03:56 +0200

    Fixed pin ex t take argv params

--- problems/pin/seneca-pin-executor-run.js
+++ problems/pin/seneca-pin-executor-run.js
@@ -6,11 +6,7 @@
  */
 var mod = require(process.argv[2])
 var seneca = require('seneca')()
-seneca.use(mod).act({role: 'math', cmd: 'sum', left: '3', right: '5'}, function (err, result) {
+seneca.use(mod).act({role: 'math', cmd: process.argv[3], left: process.argv[4], right: process.argv[5]}, function (err, result) {
   if (err) return console.error(err)
   console.log(result)
-  seneca.use(mod).act({role: 'math', cmd: 'product', left: '2', right: '15'}, function (err, result) {
-    if (err) return console.error(err)
-    console.log(result)
-  })
 })
