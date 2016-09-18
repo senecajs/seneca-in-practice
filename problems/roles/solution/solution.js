753a0d3ea8f14d7e54027c37e6520aba6d00bb14
Marco Piraccini <marco.piraccini@gmail.com>
2016-09-18 10:14:06 +0200

    ported roles

--- problems/roles/solution/solution.js
+++ problems/roles/solution/solution.js
@@ -1,10 +1,10 @@
 module.exports = function math (options) {
-  this.add({role: 'math', cmd: 'sum'}, function (msg, respond) {
+  this.add({role: 'math', cmd: 'sum'}, (msg, respond) => {
     var sum = msg.left + msg.right
     respond(null, {answer: sum})
   })
 
-  this.add({role: 'math', cmd: 'product'}, function (msg, respond) {
+  this.add({role: 'math', cmd: 'product'}, (msg, respond) => {
     var product = msg.left * msg.right
     respond(null, {answer: product})
   })
