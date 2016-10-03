635156663d57ec8ec42cd6019079873445fbda70
Michele Capra <michelecapra83@gmail.com>
2016-10-03 11:48:50 +0200

    fixed json object also in exercise

--- problems/prior_actions/solution/solution.js
+++ problems/prior_actions/solution/solution.js
@@ -1,10 +1,10 @@
 module.exports = function math (options) {
-  this.add('role:math, cmd:sum', function (msg, respond) {
+  this.add({role:'math', cmd:'sum'}, function (msg, respond) {
     var sum = msg.left + msg.right
     respond(null, {answer: sum})
   })
 
-  this.add('role:math,cmd:sum', function (msg, respond) {
+  this.add({role:'math',cmd:'sum'}, function (msg, respond) {
     this.prior(msg, function (err, out) {
       if (err) return respond(err)
 
