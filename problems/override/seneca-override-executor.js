2af65ca16f11d1823b9929242680649ea19f87e0
Daniel Knoppel (Phusion) <daniel@phusion.nl>
2016-10-19 13:11:08 +0200

    Log to stdout so that the test doesn't pass if there is an error.
    If you used Random.isFinite in the accompanying exercise, it would
    trigger an error that was ignored because verify would pass anyway
    before this patch.

--- problems/override/seneca-override-executor.js
+++ problems/override/seneca-override-executor.js
@@ -19,6 +19,6 @@ seneca.act({role: 'math', cmd: 'sum', left: 'michele', right: 2.5}, function (er
 })
 
 seneca.act({role: 'math', cmd: 'sum', left: process.argv[3], right: process.argv[4]}, function (err, result) {
-  if (err) return console.error(err)
+  if (err) return console.log(err)
   console.log(result)
 })
