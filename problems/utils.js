810bf7f616730fa2c94f26be65a2b4d963d18625
Antony74 <akb@akb.me.uk>
2021-04-08 19:58:28 +0100

    Possible fix for #106

--- problems/utils.js
+++ problems/utils.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const _ = require('lodash')
+
 exports.getRandomInt = (min = 0, max = 100) => {
   min = Math.ceil(min)
   max = Math.floor(max)
@@ -12,3 +14,13 @@ exports.getRandomFloat = (min = 0, max = 100) => {
   const num = Math.floor(Math.random() * (max - min)) + min
   return Math.round(num * 10) / 100
 }
+
+exports.pickStaticProps = (result) => {
+  const staticProps = [
+    'pattern', 'action', 'tag', 'seneca', 'version', 'timeout', 'custon', 'plugin', 'parents',
+    'remote', 'sync', 'trace', 'sub', 'data', 'err', 'err_trace', 'error', 'empty'
+  ]
+  result[1] = _.pick(result[1], staticProps)
+  return result
+}
+
